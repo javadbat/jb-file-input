9e873308809077799094b55ce1487a3718052e78
javadbat <javadbat@gmail.com>
2025-02-01 15:37:30 +0330

    feat: add build config

---
+++ build-config.ts
@@ -0,0 +1,22 @@
+import type { ReactComponentBuildConfig, WebComponentBuildConfig } from "../../tasks/build/builder/src/types.ts";
+
+export const webComponentList: WebComponentBuildConfig[] = [
+  {
+    name: "jb-file-input",
+    path: "./lib/jb-file-input.ts",
+    outputPath: "./dist/jb-file-input.js",
+    umdName: "JBFileInput",
+  },
+];
+export const reactComponentList: ReactComponentBuildConfig[] = [
+  {
+    name: "jb-file-input-react",
+    path: "./react/lib/JBFileInput.tsx",
+    outputPath: "./react/dist/JBFileInput.js",
+    external: ["jb-file-input", "prop-types", "react"],
+    globals: {
+      react: "React",
+      "prop-types": "PropTypes",
+    },
+  },
+];
